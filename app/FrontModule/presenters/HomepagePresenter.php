bdece8081ca7a44295860cc36d8bebaddc076409
Adam Navrátil <radaqs@gmail.com>
2016-10-11 21:28:47 +0200

    Zprovozněn kalendář

--- app/FrontModule/presenters/HomepagePresenter.php
+++ app/FrontModule/presenters/HomepagePresenter.php
@@ -19,6 +19,7 @@ class HomepagePresenter extends BasePresenter
     protected $scheduleManger;
     protected $taskManager;
     protected $noticeManager;
+    protected $days = array('Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota', 'Neděle');
 
 
     public function __construct(
@@ -59,9 +60,9 @@ class HomepagePresenter extends BasePresenter
         
         $groups = $this->groupManager->getUserGroups($this->activeUser);
         
-        $this->template->groups = $this->groupManager->getGroups($this->activeUser);;
+        $this->template->groups = $this->groupManager->getGroups($this->activeUser);
         $this->template->todaySchedule = $this->scheduleManger->getTodaySchedule($groups);
-        $this->template->days = array('Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota', 'Neděle');
+        $this->template->days = $this->days;
         $this->template->actualTasks = $this->taskManager->getClosestTask($groups);
         $this->template->actualNotices = $this->noticeManager->getNotices($this->activeUser, 3);
     }
@@ -74,6 +75,27 @@ class HomepagePresenter extends BasePresenter
     public function actionTimetable()
     {
         $this['topPanel']->setTitle('Rozvrh');
+        $groups = $this->groupManager->getUserGroups($this->activeUser);
+        $schedule = $this->scheduleManger->getWeekSchedule($groups);
+        
+        $maxHour = 0;
+        $minHour = 24;
+        foreach($schedule as $day) {
+            foreach($day as $hour) {
+                if($maxHour < $hour->TIME_FROM->format("%H")) {
+                    $maxHour = $hour->TIME_FROM->format("%H");
+                }
+                if($minHour > $hour->TIME_FROM->format("%H")) {
+                    $minHour = $hour->TIME_FROM->format("%H");
+                }
+            }
+        }
+        
+        $this->template->maxHour = $maxHour;
+        $this->template->minHour = $minHour;
+        $this->template->schedule = $schedule;
+        $this->template->days = $this->days;
+        
     }
     
 }
--- app/model/Manager/SchedulelManager.php
+++ app/model/Manager/SchedulelManager.php
@@ -36,4 +36,27 @@ class SchedulelManager extends BaseManager
             return array();
         }
     }
+    
+    public function getWeekSchedule($groups) 
+    {
+        $schedule = array();
+        if(!empty($groups)) {
+            $times = $this->database->query("SELECT 
+                        T1.TIME_FROM,
+                        T1.TIME_TO,
+                        T2.SHORTCUT,
+                        T2.ROOM,
+                        T1.DAY_IN_WEEK
+                    FROM group_schedule T1
+                    LEFT JOIN groups T2 ON (T1.ID_GROUP = T2.ID_GROUP AND T2.ARCHIVED=0)
+                    WHERE T1.ID_GROUP IN (" . implode(",", array_keys($groups)) . ") 
+                    ORDER BY T1.TIME_FROM ASC")->fetchAll();
+            foreach($times as $hour) {
+                $schedule[$hour->DAY_IN_WEEK][] = $hour;
+            }
+            ksort($schedule);
+            
+            return $schedule;
+        }
+    }
 }
