e20e4e0a4346e57000b3df4756227b83d2ebb877
Adam Navrátil <Radaqs@gmail.com>
2019-01-14 13:10:31 +0100

    debug off

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 define('APP_DIR', __DIR__ );
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode('2a00:1028:83a2:2252:8c36:b0b8:ec2d:238d'); // enable for your remote IP
+//$configurator->setDebugMode('2a00:1028:83a2:2252:8c36:b0b8:ec2d:238d'); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
