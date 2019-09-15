369000ac04dae661d0469c2cf6c481e27c3ae675
Petr <petr.savelka@gmail.com>
2019-09-15 18:47:52 +0200

    testy - šablony

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/.maintenance.php';
+// require __DIR__ . '/.maintenance.php';
 
 $container = require __DIR__ . '/../app/bootstrap.php';
 $container->getByType('Nette\Application\Application')->run();
